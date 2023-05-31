27773d59aa81aaa1756c23f53f483b76ddbe2c96
Balogh Szilard <105720992+BghSzilard@users.noreply.github.com>
2023-05-31 23:05:37 +0300

    added AccountController and endpoint placeholders

---
+++ RealEstateAPI/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Controllers
+{
+    [ApiController]
+    [Route("account")]
+    public class AccountController : ControllerBase
+    {
+
+        [HttpPost("register")]
+        public IActionResult Register()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("logout")]
+        public IActionResult Logout()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
