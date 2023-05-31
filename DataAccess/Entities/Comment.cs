39275474b32140e565956e38df3b483a04bd8ae5
Vitályos Norbert <vitalyosn@gmail.com>
2023-05-31 21:46:06 +0300

    added Comment entity

---
+++ DataAccess/Entities/Comment.cs
@@ -0,0 +1,11 @@
+﻿using DataAccess.Enums;
+
+namespace DataAccess.Entities
+{
+    public class Comment : BaseEntity
+    {
+        public User Poster { get; set; }
+        public Announcement Announcement { get; set; }
+        public string Message { get; set; }
+    }
+}
