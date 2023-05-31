a84a5a107643a00871b495bcb58358b0f43ed7b6
Balogh Szilard <105720992+BghSzilard@users.noreply.github.com>
2023-05-31 23:49:23 +0300

    added UserController and endpoint placeholders

---
+++ RealEstateAPI/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Controllers
+{
+    [ApiController]
+    [Route("user")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}/announcements")]
+        public IActionResult GetAnnouncements()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}/comments")]
+        public IActionResult GetComments()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
