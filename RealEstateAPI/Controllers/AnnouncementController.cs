fe94ca098e63ed07dba4dae73a0c5ec9e8f940cb
Balogh Szilard <105720992+BghSzilard@users.noreply.github.com>
2023-05-31 23:18:14 +0300

    added AnnouncementController and endpoint placeholders

---
+++ RealEstateAPI/Controllers/AnnouncementController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Controllers
+{
+    [ApiController]
+    [Route("announcement")]
+    public class AnnouncementController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public IActionResult Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
