93acd0ff3777da53d513a5dab58dc0bd011a7bf4
Vitályos Norbert <vitalyosn@gmail.com>
2023-05-31 21:45:42 +0300

    added RealEstate model

---
+++ DataAccess/Entities/RealEstate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Enums;
+
+namespace DataAccess.Entities
+{
+    public class RealEstate : BaseEntity
+    {
+        public RealEstateType Type { get; set; }
+        public int? RoomCount { get; set; }
+        public float? UsableSurfaceArea { get; set; }
+        public int? Floor { get; set; }
+        public int? ConstructionYear { get; set; }
+        public string? Address { get; set; }
+    }
+}
