55908efb79d7acdec5f9a30d8cf4a43344e35d38
Vitályos Norbert <vitalyosn@gmail.com>
2023-05-31 21:46:00 +0300

    added Announcement entity

---
+++ DataAccess/Entities/Announcement.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess.Enums;
+
+namespace DataAccess.Entities
+{
+    public class Announcement : BaseEntity
+    {
+        public TransactionType TransactionType { get; set; }
+        public RealEstate RealEstate { get; set; }
+        public decimal? Price { get; set; }
+        public User Poster { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string PostTitle { get; set; }
+        public string PostDescription { get; set; }
+
+        public IEnumerable<Comment> Comments { get; set; }
+    }
+}
