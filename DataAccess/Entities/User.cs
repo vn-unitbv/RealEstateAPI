423c9c8e1d4ac1df98ded831761dc0ae1f54785e
Vitályos Norbert <vitalyosn@gmail.com>
2023-05-31 21:44:41 +0300

    added User entity

---
+++ DataAccess/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Enums;
+
+namespace DataAccess.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public UserRole Role { get; set; }
+    }
+}
---
+++ DataAccess/Enums/UserRole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Entities;
+
+namespace DataAccess.Enums
+{
+    public enum UserRole
+    {
+        [Description("User")]
+        User,
+        [Description("Administrator")]
+        Administrator
+    }
+}
