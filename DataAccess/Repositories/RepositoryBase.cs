882f21ae4dfc8e0752ef2957daa2976981df49a2
Vitályos Norbert <vitalyosn@gmail.com>
2023-06-01 10:37:46 +0300

    added RepositoryBase

---
+++ DataAccess/Repositories/RepositoryBase.cs
@@ -0,0 +1,48 @@
+﻿using DataAccess.Entities;
+using DataLayer;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Repositories
+{
+    public class RepositoryBase<T> where T : BaseEntity
+    {
+        protected readonly AppDbContext _dbContext;
+        protected readonly DbSet<T> _dbSet;
+
+        public RepositoryBase(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<T>();
+        }
+
+        public async Task<T?> Get(Guid id)
+        {
+            return await _dbSet.FirstOrDefaultAsync(entity => entity.Id == id);
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public List<T> GetAll()
+        {
+            return GetRecords().ToList();
+        }
+
+        protected IQueryable<T> GetRecords()
+        {
+            return _dbSet.AsQueryable<T>();
+        }
+    }
+}
