0d1762f68f12bd1763f34ac3b9feb5bc5743004a
Vitályos Norbert <vitalyosn@gmail.com>
2023-06-01 10:44:45 +0300

    implemented UnitOfWork

---
+++ DataAccess/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿
+using DataAccess.Repositories;
+
+namespace DataLayer
+{
+    public class UnitOfWork
+    {
+        public UserRepository Users { get; }
+        public AnnouncementRepository Announcement { get; }
+        public CommentRepository Comment { get; }
+
+        private readonly AppDbContext _dbContext;
+
+        public UnitOfWork
+        (
+            AppDbContext dbContext,
+            UserRepository users,
+            AnnouncementRepository announcement,
+            CommentRepository comment
+        )
+        {
+            _dbContext = dbContext;
+            Users = users;
+            Announcement = announcement;
+            Comment = comment;
+        }
+
+        public void SaveChanges()
+        {
+            try
+            {
+                _dbContext.SaveChanges();
+            }
+            catch (Exception exception)
+            {
+                var errorMessage = "Error when saving to the database: "
+                    + $"{exception.Message}\n\n"
+                    + $"{exception.InnerException}\n\n"
+                    + $"{exception.StackTrace}\n\n";
+
+                Console.Error.WriteLine(errorMessage);
+            }
+        }
+    }
+}
