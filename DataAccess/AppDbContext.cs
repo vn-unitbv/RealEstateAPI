bfe758cd092958adf63e596b728f02f1514c0485
BotiOpra <botioprabodi@gmail.com>
2023-06-01 14:35:06 +0300

    extracted database connection string to appsettings

--- DataAccess/AppDbContext.cs
+++ DataAccess/AppDbContext.cs
@@ -1,43 +1,47 @@
 ﻿using DataAccess.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DataLayer
 {
-    public class AppDbContext : DbContext
-    {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder
-                    .UseSqlServer("Server=LAPTOP-GLKGU87O\\SQLEXPRESS;Database=RealEstateAPI;Trusted_Connection=True;TrustServerCertificate=true;");
-            //.LogTo(Console.WriteLine);
-        }
+	public class AppDbContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			IConfigurationRoot configuration = new ConfigurationBuilder()
+				.SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+				.AddJsonFile("appsettings.json")
+				.AddJsonFile("appsettings.Development.json")
+				.Build();
+			optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+		}
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Announcement>()
-                .Property(e => e.Price)
-                .HasPrecision(10, 2);
+			modelBuilder.Entity<Announcement>()
+				.Property(e => e.Price)
+				.HasPrecision(10, 2);
 
-            modelBuilder.Entity<RealEstate>(
-                dob =>
-                {
-                    dob.ToTable("Announcements");
-                });
+			modelBuilder.Entity<RealEstate>(
+				dob =>
+				{
+					dob.ToTable("Announcements");
+				});
 
-            modelBuilder.Entity<Announcement>(
-                ob =>
-                {
-                    ob.ToTable("Announcements");
-                    ob.HasOne(o => o.RealEstate).WithOne()
-                        .HasForeignKey<RealEstate>(o => o.Id);
-                    ob.Navigation(o => o.RealEstate).IsRequired();
-                });
-        }
+			modelBuilder.Entity<Announcement>(
+				ob =>
+				{
+					ob.ToTable("Announcements");
+					ob.HasOne(o => o.RealEstate).WithOne()
+						.HasForeignKey<RealEstate>(o => o.Id);
+					ob.Navigation(o => o.RealEstate).IsRequired();
+				});
+		}
 
-        public DbSet<User> Users { get; set; }
-        public DbSet<Announcement> Announcements { get; set; }
-        public DbSet<Comment> Comments { get; set; }
-    }
+		public DbSet<User> Users { get; set; }
+		public DbSet<Announcement> Announcements { get; set; }
+		public DbSet<Comment> Comments { get; set; }
+	}
 }
