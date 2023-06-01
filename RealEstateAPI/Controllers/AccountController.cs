86eb208c5cd750de520b9cb0f880f354a01ff904
BotiOpra <botioprabodi@gmail.com>
2023-06-01 15:00:53 +0300

    implemented Login endpoint in AccountController

--- RealEstateAPI/Controllers/AccountController.cs
+++ RealEstateAPI/Controllers/AccountController.cs
@@ -1,37 +1,47 @@
 ﻿using Core.Dtos;
 using Core.Services;
+using Infrastructure.Exceptions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Project.Controllers
 {
-    [ApiController]
-    [Route("account")]
-    public class AccountController : ControllerBase
-    {
-        private readonly UserService _userService;
+	[ApiController]
+	[Route("account")]
+	[Authorize]
+	public class AccountController : ControllerBase
+	{
+		private readonly UserService _userService;
 
-        public AccountController(UserService userService)
-        {
-            _userService = userService;
-        }
+		public AccountController(UserService userService)
+		{
+			_userService = userService;
+		}
 
-        [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody] RegisterDto registerData)
-        {
-            await _userService.Register(registerData);
-            return Ok();
-        }
+		[HttpPost("register")]
+		[AllowAnonymous]
+		public async Task<IActionResult> Register([FromBody] RegisterDto registerData)
+		{
+			await _userService.Register(registerData);
+			return Ok();
+		}
 
-        [HttpPost("login")]
-        public IActionResult Login()
-        {
-            throw new NotImplementedException();
-        }
+		[HttpPost("login")]
+		[AllowAnonymous]
+		public async Task<IActionResult> Login([FromBody] LoginDto loginData)
+		{
+			var jwtToken = await _userService.Validate(loginData);
 
-        [HttpPost("logout")]
-        public IActionResult Logout()
-        {
-            throw new NotImplementedException();
-        }
-    }
+			if (jwtToken == null)
+				throw new ForbiddenException("Invalid login credentials");
+
+			return Ok(new { token = jwtToken });
+		}
+
+		[HttpPost("logout")]
+		public IActionResult Logout()
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
