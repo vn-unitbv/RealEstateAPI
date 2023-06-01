a8a2e6bbc09c373b5ef5a249a3b01d23617ee3c4
BotiOpra <botioprabodi@gmail.com>
2023-06-01 17:20:10 +0300

    registered authentication and authorization services

--- RealEstateAPI/Program.cs
+++ RealEstateAPI/Program.cs
@@ -1,11 +1,40 @@
 using Infrastructure.Middleware;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Project.Settings;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 Dependencies.Inject(builder);
+
+// Authentication & Authorization
+builder.Services.AddAuthentication(options =>
+{
+	options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+	options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+	options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+	options.RequireHttpsMetadata = false;
+	options.SaveToken = true;
+	options.TokenValidationParameters = new TokenValidationParameters
+	{
+		ValidateIssuer = false,
+		ValidateAudience = false,
+		ValidateLifetime = true,
+		ValidateIssuerSigningKey = true,
+		ClockSkew = TimeSpan.Zero,
+
+		ValidIssuer = "RealEstateAPI-Backend",
+		ValidAudience = "RealEstateAPI-Anyone",
+		IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:SecurityKey"]!))
+	};
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -18,6 +47,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
