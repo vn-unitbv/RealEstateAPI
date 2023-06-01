04b640d06547c99351008f166d87bf60d82c61a2
Vitályos Norbert <vitalyosn@gmail.com>
2023-06-01 19:31:29 +0300

    added FeedAnnouncementDto

---
+++ Core/Dtos/FeedAnnouncementDto.cs
@@ -0,0 +1,28 @@
+﻿using DataAccess.Entities;
+using DataAccess.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Dtos
+{
+    public class FeedAnnouncementDto
+    {
+        public Guid Id { get; set; }
+        #region RealEstate
+        public RealEstateType RealEstateType { get; set; }
+        public int? RoomCount { get; set; }
+        public float? UsableSurfaceArea { get; set; }
+        public int? Floor { get; set; }
+        public int? ConstructionYear { get; set; }
+        public string? Address { get; set; }
+        #endregion
+        public TransactionType TransactionType { get; set; }
+        public decimal? Price { get; set; }
+        public string PosterName { get; set; }
+        public Guid PosterId { get; set; }
+        public string PostTitle { get; set; }
+    }
+}
