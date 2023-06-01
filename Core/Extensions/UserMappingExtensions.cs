e6baad6eeafdc2215a748b7badbce1b5a33fc0d4
Balogh Szilard <105720992+BghSzilard@users.noreply.github.com>
2023-06-01 14:05:57 +0300

    added UserMappingExtensions

---
+++ Core/Extensions/UserMappingExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Core.Dtos;
+using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Extensions
+{
+    internal static class UserMappingExtensions
+    {
+        public static IEnumerable<UserDto> ToUserDtos(this IEnumerable<User> users)
+        {
+            return users.Select(e => e.ToUserDto());
+        }
+
+        public static UserDto ToUserDto(this User user)
+        {
+            return new()
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email
+            };
+        }
+    }
+}
