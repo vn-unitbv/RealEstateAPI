c3b02ec8bc69fe1a819aa411a4b304a3bbfc46b6
Vitályos Norbert <vitalyosn@gmail.com>
2023-06-01 19:31:50 +0300

    added extension methods for mapping FeedAnnouncementDto

--- Core/Extensions/AnnouncementMappingExtensions.cs
+++ Core/Extensions/AnnouncementMappingExtensions.cs
@@ -14,15 +14,41 @@ namespace Core.Extensions
     {
         public static Announcement ToAnnouncement(this AddAnnouncementDto data)
         {
-			Announcement announcement = new()
-			{
-				TransactionType = data.TransactionType,
-				RealEstate = data.RealEstate,
-				Price = data.Price,
-				PostTitle = data.PostTitle,
-				PostDescription = data.PostDescription,
-			};
-			return announcement;
-		}
+            Announcement announcement = new()
+            {
+                TransactionType = data.TransactionType,
+                RealEstate = data.RealEstate,
+                Price = data.Price,
+                PostTitle = data.PostTitle,
+                PostDescription = data.PostDescription,
+            };
+            return announcement;
+        }
+
+        public static FeedAnnouncementDto ToFeedAnnouncementDto(this Announcement announcement)
+        {
+            FeedAnnouncementDto dto = new()
+            {
+                TransactionType = announcement.TransactionType,
+                RealEstateType = announcement.RealEstate.Type,
+                RoomCount = announcement.RealEstate.RoomCount,
+                UsableSurfaceArea = announcement.RealEstate.UsableSurfaceArea,
+                Floor = announcement.RealEstate.Floor,
+                ConstructionYear = announcement.RealEstate.ConstructionYear,
+                Address = announcement.RealEstate.Address,
+                Price = announcement.Price,
+                PostTitle = announcement.PostTitle,
+                PosterName = announcement.Poster.FirstName + " " + announcement.Poster.LastName,
+                PosterId = announcement.PosterId,
+                Id = announcement.Id
+            };
+            return dto;
+        }
+
+        public static IEnumerable<FeedAnnouncementDto> ToFeedAnnouncementDtos(
+            this IEnumerable<Announcement> announcements)
+        {
+            return announcements.Select(e => e.ToFeedAnnouncementDto());
+        }
     }
 }
