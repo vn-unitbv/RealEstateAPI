b9fe57bd0ff31e2b678fcdae25537a328882b738
Vitályos Norbert <vitalyosn@gmail.com>
2023-06-01 11:25:28 +0300

    added common exception types and ExceptionHandlingMiddleware

---
+++ Infrastructure/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,54 @@
+﻿using Infrastructure.Exceptions;
+using System.Net;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Infrastructure.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+
+        readonly RequestDelegate next;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (ForbiddenException ex)
+            {
+                await RespondToExceptionAsync(context, HttpStatusCode.Forbidden, ex.Message, ex);
+            }
+            catch (ResourceMissingException ex)
+            {
+                await RespondToExceptionAsync(context, HttpStatusCode.BadRequest, ex.Message, ex);
+            }
+            catch (Exception ex)
+            {
+                await RespondToExceptionAsync(context, HttpStatusCode.InternalServerError, "Internal Server Error", ex);
+            }
+        }
+
+        private static Task RespondToExceptionAsync(HttpContext context, HttpStatusCode failureStatusCode, string message, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)failureStatusCode;
+
+            var response = new
+            {
+                Message = message,
+                Error = exception.GetType().Name,
+                Timestamp = DateTime.UtcNow
+            };
+
+            return context.Response.WriteAsync(JsonConvert.SerializeObject(response, new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() }));
+        }
+    }
+}
--- RealEstateAPI/Program.cs
+++ RealEstateAPI/Program.cs
@@ -1,14 +1,15 @@
+using Infrastructure.Middleware;
 using Project.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 Dependencies.Inject(builder);
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
