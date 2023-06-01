c83932b9f10f712a7e1b82e7b93a017645598453
BotiOpra <botioprabodi@gmail.com>
2023-06-01 17:26:39 +0300

    added AddAnnouncementDto

---
+++ Core/Dtos/AddAnnouncementDto.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Entities;
+using DataAccess.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Dtos
+{
+	public class AddAnnouncementDto
+	{
+		public TransactionType TransactionType { get; set; }
+		public RealEstate RealEstate { get; set; }
+		public decimal? Price { get; set; }
+		public string PostTitle { get; set; }
+		public string PostDescription { get; set; }
+	}
+}
