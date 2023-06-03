11ca4477c31c946dafcc861ba72db93434a9c060
BotiOpra <botioprabodi@gmail.com>
2023-06-03 11:50:31 +0300

    Added method to query comments from database by announcement

--- DataAccess/Repositories/CommentRepository.cs
+++ DataAccess/Repositories/CommentRepository.cs
@@ -1,11 +1,22 @@
 ﻿using DataAccess.Entities;
 using DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 
 namespace DataAccess.Repositories;
 
-public class CommentRepository : RepositoryBase<Announcement>
+public class CommentRepository : RepositoryBase<Comment>
 {
     public CommentRepository(AppDbContext dbContext) : base(dbContext)
     {
+    }
+
+    public async Task<IEnumerable<Comment>> GetCommentsByAnnouncement(Guid announcementId)
+    {
+	    var results = await _dbSet
+		    .Where(c => c.Announcement.Id == announcementId)
+		    .ToListAsync();
+
+	    return results;
     }
 }
