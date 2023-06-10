11d25a51c9df9b9525bd5e7aa90088f6b2a66263
Balogh Szilard <105720992+BghSzilard@users.noreply.github.com>
2023-06-10 12:11:17 +0300

    created Dtos for filtering and updating announcements

---
+++ Core/Dtos/AnnouncementFilterDto.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Core.Dtos
+{
+    public class AnnouncementFilterDto
+    {
+        public TransactionType? TransactionType { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public RealEstateType? Type { get; set; }
+        public int? MinRoomCount { get; set; }
+        public int? MaxRoomCount { get; set; }
+        public int? MinUsableSurfaceArea { get; set; }
+        public int? MaxUsableSurfaceArea { get; set; }
+    }
+}
---
+++ Core/Dtos/UpdateAnnouncementDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Dtos
+{
+    public class UpdateAnnouncementDto
+    {
+        public decimal? Price { get; set; }
+        public string PostTitle { get; set; }
+        public string PostDescription { get; set; }
+    }
+}
